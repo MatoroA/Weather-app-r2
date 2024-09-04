7bafd4a183e7a9df2e10c9f46d4c1ad067fd74d5
Andani Matoro <andanimatoro@gmail.com>
2024-09-05 00:30:02 +0200

    weather repository

---
+++ core/data/src/main/java/dvt/com/weather/data/WeatherRepository.kt
@@ -0,0 +1,9 @@
+package dvt.com.weather.data
+
+import dvt.com.weather.model.weather.WeatherForecast
+import dvt.com.weather.network.WeatherLocationParam
+import kotlinx.coroutines.flow.Flow
+
+interface WeatherRepository {
+    fun getWeatherForecast(locationParam: WeatherLocationParam): Flow<WeatherForecast>
+}
---
+++ core/data/src/main/java/dvt/com/weather/data/WeatherRepositoryImpl.kt
@@ -0,0 +1,46 @@
+package dvt.com.weather.data
+
+import dvt.com.weather.model.weather.Main
+import dvt.com.weather.model.weather.Weather
+import dvt.com.weather.model.weather.WeatherForecast
+import dvt.com.weather.network.WeatherDataSource
+import dvt.com.weather.network.WeatherLocationParam
+import dvt.com.weather.network.model.NetworkWeatherForecast
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class WeatherRepositoryImpl @Inject constructor(
+    private val dataSource: WeatherDataSource,
+) : WeatherRepository {
+    override fun getWeatherForecast(locationParam: WeatherLocationParam): Flow<WeatherForecast> =
+        flow {
+            emit(
+                dataSource.getWeather(locationParam).toExternalModel()
+            )
+        }
+
+    private fun NetworkWeatherForecast.toExternalModel(): WeatherForecast {
+        return WeatherForecast(
+            weather = this.weather.map {
+                Weather(
+                    id = it.id,
+                    main = it.main,
+                    icon = it.icon,
+                    description = it.description,
+                )
+            },
+            main = Main(
+                temperatureMax = this.main.temperatureMax,
+                temperatureMin = main.temperatureMin,
+                temperature = main.temperature,
+                pressure = main.pressure,
+                humidity = main.humidity,
+                seaLeve = main.seaLeve,
+                groundLevel = main.groundLevel,
+                feelsLike = main.feelsLike,
+            )
+        )
+    }
+
+}
