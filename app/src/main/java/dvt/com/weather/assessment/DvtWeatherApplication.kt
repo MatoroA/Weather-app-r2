5a204d88007aa914f671b9af1f7d86da1762fb0f
Andani Matoro <andanimatoro@gmail.com>
2024-09-07 23:49:09 +0200

    added hilt

---
+++ app/src/main/java/dvt/com/weather/assessment/DvtWeatherApplication.kt
@@ -0,0 +1,9 @@
+package dvt.com.weather.assessment
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class DvtWeatherApplication : Application() {
+}
