4751592e87ea689db90279daf81ca25a182a9f98
Andani Matoro <andanimatoro@gmail.com>
2024-10-08 23:33:57 +0200

    refactor

--- app/src/main/java/dvt/com/weather/assessment/ui/MainActivityViewModel.kt
+++ app/src/main/java/dvt/com/weather/assessment/ui/MainActivityViewModel.kt
@@ -3,19 +3,19 @@ package dvt.com.weather.assessment.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dvt.com.weather.data.util.UserLocation
+import dvt.com.weather.data.util.LocationTemperature
 import dvt.com.weather.model.CurrentLocation
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainActivityViewModel @Inject constructor(
-    private val userLocation: UserLocation,
+    private val locationTemperature: LocationTemperature,
 ) : ViewModel() {
 
     fun onUpdateLocation(currentLocation: CurrentLocation?) {
         viewModelScope.launch {
-            userLocation.onLocationUpdate(currentLocation)
+            locationTemperature.onLocationUpdate(currentLocation)
         }
     }
 }
