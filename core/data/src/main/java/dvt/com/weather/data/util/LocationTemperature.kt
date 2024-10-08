fd2315ff1999dbc388a96aeb4150b2e7dd973f7b
Andani Matoro <andanimatoro@gmail.com>
2024-10-08 23:06:02 +0200

    location temperature

--- core/data/src/main/java/dvt/com/weather/data/util/LocationTemperature.kt
+++ core/data/src/main/java/dvt/com/weather/data/util/LocationTemperature.kt
@@ -1,12 +1,13 @@
 package dvt.com.weather.data.util
 
 import dvt.com.weather.model.CurrentLocation
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.StateFlow
 
-interface UserLocation {
+interface LocationTemperature {
     val location: SharedFlow<CurrentLocation?>
+    val temperature: SharedFlow<Double>
 
     suspend fun onLocationUpdate(location: CurrentLocation?)
+
+    suspend fun temperature(temperature: Double)
 }
--- core/data/src/main/java/dvt/com/weather/data/util/LocationTemperatureImpl.kt
+++ core/data/src/main/java/dvt/com/weather/data/util/LocationTemperatureImpl.kt
@@ -1,18 +1,25 @@
 package dvt.com.weather.data.util
 
 import dvt.com.weather.model.CurrentLocation
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import javax.inject.Inject
 
-class UserCurrentLocationImpl @Inject constructor() : UserLocation {
+class LocationTemperatureImpl @Inject constructor() : LocationTemperature {
 
     private val _location = MutableSharedFlow<CurrentLocation?>()
     override val location: SharedFlow<CurrentLocation?> = _location
 
+    private val _temperature = MutableSharedFlow<Double>()
+    override val temperature: SharedFlow<Double> = _temperature
+
     override suspend fun onLocationUpdate(location: CurrentLocation?) {
         _location.emit(location)
     }
+
+    override suspend fun temperature(temperature: Double) {
+        _temperature.emit(temperature)
+    }
+
+
 }
