c07122d2c3ea03a64fc187763c346c4a6fcccf2c
Andani Matoro <andanimatoro@gmail.com>
2024-10-10 00:17:52 +0200

    dependencies refactor

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -16,11 +16,10 @@ android {
 
 dependencies {
     implementation(libs.kotlinx.serialization.json)
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
-    implementation("com.google.code.gson:gson:2.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation(libs.okhttp)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.retrofit.serialisation.converter)
+    implementation(libs.retrofit)
+    implementation(libs.google.gson)
+    implementation(libs.okhttp.logging.intercerptor)
 }
