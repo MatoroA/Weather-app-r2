f8af1084bee3fb052dfb88ee2ed07472b7454844
Andani Matoro <andanimatoro@gmail.com>
2024-10-12 12:12:34 +0200

    refactor

--- core/data/src/main/java/dvt/com/weather/data/util/LiveWeatherImpl.kt
+++ core/data/src/main/java/dvt/com/weather/data/util/LiveWeatherImpl.kt
@@ -1,18 +1,22 @@
 package dvt.com.weather.data.util
 
 import dvt.com.weather.model.weather.CurrentWeather
-import dvt.com.weather.model.weather.Weather
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class LiveWeatherImpl @Inject constructor() : LiveWeather {
-
-
+class LiveWeatherImpl @Inject constructor(
+    private val dispatcherIO: CoroutineDispatcher = Dispatchers.IO,
+) : LiveWeather {
     private val _weather = MutableSharedFlow<CurrentWeather>()
     override val weather: SharedFlow<CurrentWeather> = _weather
 
     override suspend fun liveWeather(weather: CurrentWeather) {
-        _weather.emit(weather)
+        withContext(dispatcherIO) {
+            _weather.emit(weather)
+        }
     }
 }
