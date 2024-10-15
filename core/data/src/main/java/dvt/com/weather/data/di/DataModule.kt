8ee5ffc291d053726a547558aa6507012f8ae37f
Andani Matoro <andanimatoro@gmail.com>
2024-10-16 01:07:56 +0200

    refactor

--- core/data/src/main/java/dvt/com/weather/data/di/DataModule.kt
+++ core/data/src/main/java/dvt/com/weather/data/di/DataModule.kt
@@ -2,17 +2,14 @@ package dvt.com.weather.data.di
 
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import dvt.com.weather.data.repository.WeatherRepository
 import dvt.com.weather.data.repository.WeatherRepositoryImpl
-import dvt.com.weather.data.util.DvtLocationManagerImpl
-import dvt.com.weather.data.util.DvtLocationManager
-import dvt.com.weather.data.util.LiveWeather
-import dvt.com.weather.data.util.LiveWeatherImpl
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
+import dvt.com.weather.data.util.LiveLocationManagerImpl
+import dvt.com.weather.data.util.LiveLocationManager
+import dvt.com.weather.data.util.LiveWeatherManager
+import dvt.com.weather.data.util.LiveWeatherManagerImpl
 import javax.inject.Singleton
 
 @Module
@@ -24,9 +21,9 @@ interface DataModule {
 
     @Singleton
     @Binds
-    fun bindsUserLocation(impl: DvtLocationManagerImpl): DvtLocationManager
+    fun bindsUserLocation(impl: LiveLocationManagerImpl): LiveLocationManager
 
     @Singleton
     @Binds
-    fun bindsLiveWeather(impl: LiveWeatherImpl): LiveWeather
+    fun bindsLiveWeather(impl: LiveWeatherManagerImpl): LiveWeatherManager
 }
