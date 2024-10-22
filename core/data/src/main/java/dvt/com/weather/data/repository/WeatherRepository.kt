90702d056800ae872b92afff78bdce7d023bef9b
Andani Matoro <andanimatoro@gmail.com>
2024-10-23 00:08:40 +0200

    refactor

--- core/data/src/main/java/dvt/com/weather/data/repository/WeatherRepository.kt
+++ core/data/src/main/java/dvt/com/weather/data/repository/WeatherRepository.kt
@@ -1,10 +1,15 @@
 package dvt.com.weather.data.repository
 
+import dvt.com.weather.model.LocationCoordinates
 import dvt.com.weather.model.weather.Forecast
 import dvt.com.weather.model.weather.CurrentWeather
 import kotlinx.coroutines.flow.Flow
 
+
 interface WeatherRepository {
-    fun getCurrentWeather(longitude: Double, latitude: Double): Flow<CurrentWeather>
-    fun getWeatherForecast(longitude: Double, latitude: Double): Flow<List<Forecast>>
+    fun getCurrentWeather(coordinates: LocationCoordinates): Flow<CurrentWeather>
+
+    fun getWeatherForecast(coordinates: LocationCoordinates): Flow<List<Forecast>>
+
+    suspend fun sync(coordinates: LocationCoordinates): Boolean
 }
