7805d839d78879de6627e84ad6dd0def815623ec
Andani Matoro <andanimatoro@gmail.com>
2024-10-24 01:24:56 +0200

    register a class that listens to location change, and trigger network call

--- app/src/main/java/dvt/com/weather/assessment/DvtWeatherApplication.kt
+++ app/src/main/java/dvt/com/weather/assessment/DvtWeatherApplication.kt
@@ -2,7 +2,19 @@ package dvt.com.weather.assessment
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import dvt.com.weather.data.util.CompositeLocationWeatherWorker
+import javax.inject.Inject
 
 
 @HiltAndroidApp
-class DvtWeatherApplication : Application()
+class DvtWeatherApplication : Application() {
+
+    @Inject
+    lateinit var compositeLocationWeatherWorker: CompositeLocationWeatherWorker
+
+    override fun onCreate() {
+        super.onCreate()
+
+        compositeLocationWeatherWorker()
+    }
+}
